d76fd5993a3917c84822632ce300984bfef1ef4f
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-03-04 20:38:53 +0530

    expand

---
+++ If-Else/Nested if.php
@@ -0,0 +1,14 @@
+<?php
+    $x=68;
+    if($x>75){
+        echo "You got A Pass";
+    }else if($x>65){
+        echo "You got B Pass";
+    }else if($x>55){
+        echo "You got C Pass";
+    }else if($x>35){
+        echo "You got S Pass";
+    }else{
+        echo "You are Fail";
+    }
+?>
