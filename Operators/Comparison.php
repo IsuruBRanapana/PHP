896237be8cad0d09d5ab6bdc11ee482d062c731a
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-03-06 23:46:09 +0530

    Learn comparison operator

---
+++ Operators/Comparison.php
@@ -0,0 +1,9 @@
+<?php
+    $number1=10;
+    $number2=45;
+    if($number1<$number2){
+        echo "Number1 is less than Number2";
+    }else{
+        echo "Number2 is less than Number1";
+    }
+?>
