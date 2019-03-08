c3deaabfc0d2aadfc2a58e5115a1603888b2cc63
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-03-08 22:19:14 +0530

    Learn Switch-Case

---
+++ Switch Case/Switch.php
@@ -0,0 +1,26 @@
+<?php
+	$x=9;
+	switch($x){
+		case 1:
+			echo "One";
+			break;
+		case 2:
+			echo "Two";
+			break;
+		case 3:
+			echo "Three";
+			break;
+		case 4:
+			echo "Four";
+			break;
+		case 5:
+			echo "Five";
+			break;
+		case 6:
+			echo "Six";
+			break;
+		default:
+			echo "Out of range";
+			break;
+	}
+?>
