699cd8c5926011d836fd38dced00c8d1489cc862
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-03-08 20:11:30 +0530

    Learn triple equal operator

---
+++ Operators/TripleEqual.php
@@ -0,0 +1,18 @@
+<?php
+	$num1='10';
+	$num2=10;
+	
+	//consider about image or value
+	if($num1==$num2){
+		echo "Equal.<br>";
+	}else{
+		echo "Not Equal.<br>";
+	}
+	
+	//consider about image , value and data type use ===
+	if($num1===$num2){
+		echo "Equal.<br>";
+	}else{
+		echo "Not Equal.<br>";
+	}
+?>
