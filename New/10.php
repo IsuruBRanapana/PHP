640cb150b56add194fbece802081f927181a53f9
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-26 00:32:33 +0530

    Updated

---
+++ New/10.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+
+		$user="A";
+		switch ($user) {
+			case 'A':
+				echo "A";
+				break;
+			case 'B':
+				echo "B";
+				break;
+			case 'C':
+				echo "C";
+				break;
+			default:
+				echo "Invalid User Group";
+				break;
+		}
+	 ?>
+</body>
+</html>
